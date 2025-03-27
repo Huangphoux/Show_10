42ecf1ab1204cf7f569e36d24cabddacab22e257
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-03-27 17:12:59 +0700

    Rename project files

--- show10/Program.cs
+++ show10/Program.cs
@@ -11,10 +11,7 @@ namespace show10
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-#pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-            Application.SetColorMode(SystemColorMode.System);
-#pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
--- show10/Windows/MainWindow.cs
+++ show10/Windows/MainWindow.cs
@@ -1,8 +1,8 @@
 namespace show10
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
