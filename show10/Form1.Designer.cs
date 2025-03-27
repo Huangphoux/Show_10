3dec3b868054756cb658dcefba30596c34be9f47
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-03-27 16:52:54 +0700

    Create template and gitignore

---
+++ show10/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace show10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button_Test = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // button_Test
+            // 
+            button_Test.Location = new Point(83, 65);
+            button_Test.Name = "button_Test";
+            button_Test.Size = new Size(94, 29);
+            button_Test.TabIndex = 0;
+            button_Test.Text = "button1";
+            button_Test.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(83, 118);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(101, 24);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(checkBox1);
+            Controls.Add(button_Test);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button_Test;
+        private CheckBox checkBox1;
+    }
+}
