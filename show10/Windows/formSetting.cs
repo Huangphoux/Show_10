bba66471d54863281684380b758798f9f172fc25
Hip2k5 <hoanghiepta2005@gmail.com>
2025-04-21 11:04:20 +0700

    SideBar (Full)
    T lm r tụi m xem coi code chạy thử dc trên máy tụi m ko

---
+++ show10/Windows/formSetting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Show10.Windows
+{
+    public partial class formSetting : Form
+    {
+        public formSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void formSetting_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
