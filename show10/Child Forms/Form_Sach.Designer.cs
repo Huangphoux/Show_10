31521f1a1766c098a96a42f437d001dc5e68aa6b
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-05-12 14:21:49 +0700

    Delete unused dark components

--- show10/Child Forms/Form_Sach.Designer.cs
+++ show10/Child Forms/Form_Sach.Designer.cs
@@ -23,18 +23,47 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            groupBox_Sach = new GroupBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // groupBox_Sach
+            // 
+            groupBox_Sach.Dock = DockStyle.Top;
+            groupBox_Sach.Location = new Point(0, 0);
+            groupBox_Sach.Name = "groupBox_Sach";
+            groupBox_Sach.Size = new Size(782, 355);
+            groupBox_Sach.TabIndex = 0;
+            groupBox_Sach.TabStop = false;
+            groupBox_Sach.Text = "Nhập liệu";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(163, 398);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 1;
             // 
             // Form_Sach
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(782, 553);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox_Sach);
             Name = "Form_Sach";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Quản lý sách";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox_Sach;
+        private DataGridView dataGridView1;
     }
 }
